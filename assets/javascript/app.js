3a6d4c11e1ee329a674019006ffe3996c964f9b7
Mark B <mark.bullard42@gmail.com>
2019-06-25 09:48:16 -0400

    firebase config added to js file

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+// Firebase Config
+var firebaseConfig = {
+  apiKey: "AIzaSyD-jmpCERf4Yo4Gm3P7IAuzZAPGFST0omE",
+  authDomain: "my-first-porject-e0b87.firebaseapp.com",
+  databaseURL: "https://my-first-porject-e0b87.firebaseio.com",
+  projectId: "my-first-porject-e0b87",
+  storageBucket: "my-first-porject-e0b87.appspot.com",
+  messagingSenderId: "203434104992",
+  appId: "1:203434104992:web:e7689d129391f116"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
